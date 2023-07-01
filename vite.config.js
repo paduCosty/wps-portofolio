5ee37cd33445c4839f9a0aa66760bf088321acae
paduCosti <padu.costi7@gmail.com>
2023-07-01 20:31:03 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
             template: {
                 transformAssetUrls: {
                     base: 'https://vault.webpad-solutions.com/',
-                    includeAbsolute: false,
+                    includeAbsolute: true,
                 },
             },
         }),
