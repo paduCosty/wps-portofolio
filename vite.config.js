434a39d077b8ea4a508c841dcbfbc78ef9958706
paduCosti <padu.costi7@gmail.com>
2023-07-01 23:34:48 +0300

    s

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
-
+const host = 'dev.webpad-solutions.com';
 export default defineConfig({
     plugins: [
         laravel({
