a5bbf5c4ed722e902889dfcc59b5a5dd11dd781b
Bianca <biabiancaalbertina@yahoo.com>
2023-08-05 15:59:04 +0300

    Home Style

--- resources/js/Pages/Home/Home.vue
+++ resources/js/Pages/Home/Home.vue
@@ -1,12 +1,191 @@
 <template>
+    <div style="padding-left: 39px; margin-top: 69px">
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="380"
+            height="84"
+            viewBox="0 0 792 134"
+        >
+            <text
+                id="Web_Pad_Solution"
+                data-name="Web Pad Solution"
+                transform="translate(0 109)"
+                fill="#fff"
+                font-size="101"
+                font-family="SegoeUI, Segoe UI"
+                opacity="0.7"
+            >
+                <tspan x="0" y="0">Web Pad Solution</tspan>
+            </text>
+        </svg>
+    </div>
 
-    asdf
+    <div style="padding-left: 53px">
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="611"
+            height="98"
+            viewBox="0 0 611 98"
+        >
+            <text
+                id="Provides_web_application_development_and_software_creation...."
+                data-name="Provides web application 
+                development and software creation...."
+                transform="translate(0 40)"
+                fill="#c9c9c9"
+                font-size="20"
+                font-family="SegoeUI, Segoe UI"
+                opacity="0.75"
+            >
+                <tspan x="0" y="0">Provides web application</tspan>
+                <tspan x="0" y="30">
+                    development and software creation....
+                </tspan>
+            </text>
+        </svg>
+    </div>
+
+    <div style="padding-left: 910px; margin-top: -189px">
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="284"
+            height="240"
+            viewBox="0 0 354 240"
+        >
+            <g
+                id="Rectangle_174"
+                data-name="Rectangle 174"
+                stroke="#707070"
+                stroke-width="1"
+                opacity="0.15"
+            >
+                <rect width="354" height="240" stroke="none" />
+                <rect x="0.5" y="0.5" width="353" height="337" fill="none" />
+            </g>
+        </svg>
+    </div>
+
+    <div style="padding-left: 930px; margin-top: -250px">
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="6"
+            height="212"
+            viewBox="0 0 10 202"
+        >
+            <path
+                id="Path_178"
+                data-name="Path 178"
+                d="M4,0,1.2,295.895,0,422"
+                transform="translate(3.5 0.033)"
+                fill="none"
+                stroke="#fff"
+                stroke-width="7"
+                opacity="0.5"
+            />
+        </svg>
+    </div>
+
+    <div style="padding-left: 960px; margin-top: -170px">
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="364"
+            height="290"
+            viewBox="0 0 364 290"
+        >
+            <text
+                id="Web_software_development_to_customize_orders._Web_hosting_services._Maintenance_and_security._Custom_design."
+                data-name="Web software development to customize orders.
+ 
+Web hosting services. 
+
+Maintenance and security.
+ 
+Custom design."
+                fill="#fff"
+                font-size="16"
+                font-family="SegoeUI, Segoe UI"
+                opacity="0.9"
+            >
+                <tspan x="0" y="19">Web software development to</tspan>
+                <tspan x="0" y="38">customize orders.</tspan>
+                <!-- <tspan x="0" y="49"></tspan> -->
+                <tspan x="0" y="78">Web hosting services.</tspan>
+                <!-- <tspan x="0" y="85"></tspan> -->
+                <tspan x="0" y="118">Maintenance and security.</tspan>
+                <!-- <tspan x="0" y="361"></tspan> -->
+                <tspan x="0" y="158">Custom design.</tspan>
+            </text>
+        </svg>
+    </div>
+
+    <!-- <div style="padding-left: 680px; margin-top: -169px">
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="10"
+            height="322.066"
+            viewBox="0 0 11 322.066"
+        >
+            <path
+                id="Path_178"
+                data-name="Path 178"
+                d="M4,0,1.2,295.895,0,422"
+                transform="translate(3.5 0.033)"
+                fill="none"
+                stroke="#fff"
+                stroke-width="7"
+                opacity="0.5"
+            />
+        </svg>
+
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="454"
+            height="338"
+            viewBox="-5 -10 454 338"
+        >
+            <g
+                id="Rectangle_174"
+                data-name="Rectangle 174"
+                stroke="#707070"
+                stroke-width="1"
+                opacity="0.15"
+            >
+                <rect width="454" height="338"  stroke="none" />
+                <rect x="0.5" y="0.5" width="453" height="337" fill="none" />
+            </g>
+
+            <text
+                id="Web_software_development_to_customize_orders._Web_hosting_services._Maintenance_and_security._Custom_design."
+                data-name="Web software development to customize orders.
+ 
+                    Web hosting services. 
+
+                    Maintenance and security.
+ 
+                    Custom design."
+                fill="#fff"
+                font-size="20"
+                font-family="SegoeUI, Segoe UI"
+                opacity="0.9"
+            >
+                <tspan x="25" y="19">Web software development to</tspan>
+                <tspan x="0" y="47">customize orders.</tspan>
+                <tspan x="0" y="79"></tspan>
+                 <tspan x="0" y="62">Web hosting services.</tspan> -->
+    <!-- <tspan x="0" y="75"></tspan> -->
+    <!-- <tspan x="0" y="89">Maintenance and security.</tspan> -->
+    <!-- <tspan x="0" y="99"></tspan> -->
+    <!-- <tspan x="0" y="114">Custom design.</tspan> -->
+    <!-- </text>
+        </svg>
+    </div>  -->
 </template>
 
 <script>
+// import "../Assets/home.css";
 export default {
-    name: 'Home',
-    components: {
-    },
+    name: "Home",
+    components: {},
 };
 </script>
+<style></style>
