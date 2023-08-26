c423ceb5f3c6a0c106aa2c524d4d09b3d7c6ce83
Bianca <biabiancaalbertina@yahoo.com>
2023-08-27 00:19:14 +0300

    About Style-1

--- resources/js/Pages/About/About.vue
+++ resources/js/Pages/About/About.vue
@@ -73,4 +73,75 @@
             </svg>
         </div>
     </div>
+
+    <div style="margin-top: -14.5%; margin-left: 45%">
+        <div>
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="729"
+                height="308"
+                viewBox="0 0 729 568"
+            >
+                <g
+                    id="Rectangle_172"
+                    data-name="Rectangle 172"
+                    stroke="#707070"
+                    stroke-width="1"
+                    opacity="0.15"
+                >
+                    <rect width="729" height="568" stroke="none" />
+                    <rect
+                        x="0.5"
+                        y="0.5"
+                        width="728"
+                        height="567"
+                        fill="none"
+                    />
+                </g>
+            </svg>
+        </div>
+
+        <div style="margin-top: -39%">
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="770"
+                height="81"
+                viewBox="0 0 476 121"
+            >
+                <text
+                    id="team_data"
+                    data-name="team data"
+                    fill="#fff"
+                    font-size="82"
+                    font-family="SegoeUI, Segoe UI"
+                    opacity="0.9"
+                >
+                    <tspan x="0" y="88">Team Data</tspan>
+                </text>
+            </svg>
+        </div>
+
+        <div style="margin-top: 0.4%">
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="988"
+                height="94"
+                viewBox="0 0 644 104"
+            >
+                <text
+                    id="Provides_web_application_development_and_software_creation...."
+                    data-name="Provides web application development and software creation...."
+                    transform="translate(0 42)"
+                    fill="#c9c9c9"
+                    font-size="22"
+                    font-family="SegoeUI, Segoe UI"
+                    opacity="0.75"
+                >
+                    <tspan x="0" y="0">Provides web application</tspan>
+                    <tspan x="0" y="37">development and software creation....
+                    </tspan>
+                </text>
+            </svg>
+        </div>
+    </div>
 </template>
